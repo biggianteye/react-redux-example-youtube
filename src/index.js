56b011766257910913c10dc4ad4df6aea6dfb108
Burhan Ali <burhan.ali@graze.com>
2017-12-29 19:35:11 +0000

    Make App class based and add some initial search data

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM  from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar';
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    )
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: []
+        };
+
+        YTSearch({key: YOUTUBE_API_KEY, term: 'cheesecake' }, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        )
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
